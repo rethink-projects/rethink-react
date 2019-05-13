55ef6b90bc9c770c046e134c1a82d981b8ed9377
Arthur Vargas <arthur@rethink.ws>
2019-05-13 15:34:32 -0300

    Create a screen inside a module #2

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,4 +4,10 @@ const handleStrings = (fn, string) => {
   return end
 }
 
-module.exports = { handleStrings }
+const handleScreenName = (fn, string) => {
+  const text = fn.trim(string);
+  const end = fn.upperFirst(text);
+  return end.indexOf("Screen") === -1 ? `${end}Screen` : end;
+}
+
+module.exports = { handleStrings, handleScreenName }
